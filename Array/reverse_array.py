eaf9c16203ba179b242a73665057a2c004c3ad59
shreyasrikanth2 <shreya2310275@ssn.edu.in>
2025-12-12 09:25:41 +0530

    Create reverse_array.py

---
+++ Array/reverse_array.py
@@ -0,0 +1,9 @@
+def reversearray(arr):
+    left=0
+    right=len(arr)-1
+    while left<right:
+        arr[left],arr[right]=arr[right],arr[left]
+        left=left+1
+        right=right-1
+    return arr
+print(reversearray([1, 4, 3, 2, 6, 5]))
