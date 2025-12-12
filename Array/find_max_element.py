b8df1e16f4384d1765d5f8e22f8479039ec8fbfb
shreyasrikanth2 <shreya2310275@ssn.edu.in>
2025-12-12 10:02:44 +0530

    Update find_max_element.py

--- Array/find_max_element.py
+++ Array/find_max_element.py
@@ -2,7 +2,7 @@ def findmax(arr):
     maxele=arr[0]
     n=len(arr)-1
     for a in range(n):
-        if maxele<arr[n]:
+        if maxele<arr[a]:
             maxele=arr[n]
     return maxele
 print(findmax([3, 5, 4, 1, 9]))
